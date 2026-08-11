31d17ea6f7f8ec0bff00bade378932fb2fb84b27
Auto Commit <auto@local.invalid>
2026-08-11 16:35:50 +0500

    Configure admin API with environment variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
-  
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 API.interceptors.request.use((config) => {
@@ -17,6 +16,7 @@ API.interceptors.request.use((config) => {
 
 API.interceptors.response.use(
   (response) => response,
+
   (error) => {
     if (error.response?.status === 401) {
       localStorage.removeItem("token");
