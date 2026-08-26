286c0dcd15c2921f6e8e00acff03fc08618bb6fd
Auto Commit <auto@local.invalid>
2026-08-26 21:00:02 +0500

    restore working sustainability event flow

--- src/config/menuItems.js
+++ src/config/menuItems.js
@@ -44,11 +44,6 @@ const menuItems = [
     path: "/employees",
   },
   {
-    title: "Sustainability Events",
-    icon: FaLeaf,
-    path: "/sustainability-events",
-  },
-  {
     title: "Investors",
     icon: FaChartLine,
     path: "/investors",
